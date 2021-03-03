7332de0eaff3c75218f237fd4fcf1d70ad625f48
todocodo <todomodo12@gmail.com>
2021-03-03 15:02:51 +0000

    'commit_before_build'

--- src/components/features.js
+++ src/components/features.js
@@ -31,6 +31,7 @@ export default () => (
               <iframe className="video-add"
                 title={value.videoTitle}
                 src={value.vimeo}
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 frameBorder="0"
                 webkitallowfullscreen="true"
                 mozallowfullscreen="true"
